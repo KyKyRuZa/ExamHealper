76a76a2b710a4a5228b6d655fd6c7d5403a4ba64
+Ky <81905624+KyKyRuZa@users.noreply.github.com>
2024-01-06 13:24:45 +0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+const navbarToggle = navbar.querySelector("#navbar-toggle");
+const navbarMenu = document.querySelector("#navbar-menu");
+const navbarLinksContainer = navbarMenu.querySelector(".navbar-links");
+let isNavbarExpanded = navbarToggle.getAttribute("aria-expanded") === "true";
+
+const toggleNavbarVisibility = () => {
+  
+  isNavbarExpanded = !isNavbarExpanded;
+  navbarToggle.setAttribute("aria-expanded", isNavbarExpanded);
+}
+;
+
+navbarToggle.addEventListener("click", toggleNavbarVisibility);
+
+navbarLinksContainer.addEventListener("click", (e) => e.stopPropagation());
+navbarMenu.addEventListener("click", toggleNavbarVisibility);
+
+
+function changeIcon() {
+  var icon = document.getElementById('icon');
+  icon.classList.toggle('bx-star');
+  icon.classList.toggle('bxs-star');
+}
